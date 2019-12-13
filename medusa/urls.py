124ed0231d25b00e01a975e602710a58ee4b2732
simonfroggatt <simon@totalsafetygroup.co.uk>
2019-12-13 13:53:19 +0000

    dashboard

--- medusa/urls.py
+++ medusa/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('json_product_data/', product_list_asJson),
-    path('', order_stats),
-    #path('', TemplateView.as_view(template_name='index.html'), name='home')
+    #path('', order_stats),
+    path('', TemplateView.as_view(template_name='index.html'), name='home')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
